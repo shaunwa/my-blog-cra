c3dcbb28a078a9ba82f2cacb78aebde7f86f49e9
Shaun Wassell <shaun@trove.ai>
2019-03-22 15:56:16 -0400

    After 01.08

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import articles from './article-content';
+import ArticlesList from '../components/ArticlesList';
 
 const ArticlePage = ({ match }) => {
     const name = match.params.name;
-    const article = articles.find(article => article.name === name);
+    const currentArticle = articles.find(article => article.name === name);
     
-    if (!article) return <h1>Oops! Article not found</h1>
+    if (!currentArticle) return <h1>Oops! Article not found</h1>
+
+    const otherArticles = articles.filter(article => article.name !== currentArticle.name);
 
     return (
         <div>
-            <h1>{article.title}</h1>
-            {article.content.map((paragraph, key) => <p key={key}>{paragraph}</p>)}
+            <h1>{currentArticle.title}</h1>
+            {currentArticle.content.map((paragraph, key) => <p key={key}>{paragraph}</p>)}
+            <h3>Other Articles:</h3>
+            <ArticlesList articles={otherArticles} />
         </div>
     );
 }
--- src/pages/ArticlesListPage.js
+++ src/pages/ArticlesListPage.js
@@ -1,16 +1,11 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import articles from './article-content';
+import ArticlesList from '../components/ArticlesList';
 
 const ArticlesListPage = () => (
     <>
         <h1>Articles</h1>
-        {articles.map(article => (
-            <Link to={`/article/${article.name}`} key={article.name}>
-                <h3>{article.title}</h3>
-                <p>{article.content[0].substring(0, 150)}...</p>
-            </Link>
-        ))}
+        <ArticlesList articles={articles} />
     </>
 );
 
