c765d173ac8afe9939b51765e99f1030556a1d03
Shaun Wassell <shaun@trove.ai>
2019-03-22 14:33:12 -0400

    After 01.07

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import articles from './article-content';
 
 const ArticlePage = ({ match }) => {
-    const { params: { name } } = match;
-    return <h1>You're on the {name} page</h1>;
+    const name = match.params.name;
+    const article = articles.find(article => article.name === name);
+    
+    if (!article) return <h1>Oops! Article not found</h1>
+
+    return (
+        <div>
+            <h1>{article.title}</h1>
+            {article.content.map((paragraph, key) => <p key={key}>{paragraph}</p>)}
+        </div>
+    );
 }
 
 export default ArticlePage;
--- src/pages/ArticlesListPage.js
+++ src/pages/ArticlesListPage.js
@@ -1,6 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import articles from './article-content';
 
-const ArticlesListPage = () =>
-    <h1>You're on the articles list page</h1>;
+const ArticlesListPage = () => (
+    <>
+        <h1>Articles</h1>
+        {articles.map(article => (
+            <Link to={`/article/${article.name}`} key={article.name}>
+                <h3>{article.title}</h3>
+                <p>{article.content[0].substring(0, 150)}...</p>
+            </Link>
+        ))}
+    </>
+);
 
 export default ArticlesListPage;
